dae3b4cbd44b9866b7a19e29cebdefb793d0f459
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-08-14 23:04:06 +0200

    clojure.xml/parse

--- src/main/clojure/pl/tomaszgigiel/abc_xml/core.clj
+++ src/main/clojure/pl/tomaszgigiel/abc_xml/core.clj
@@ -1,6 +1,11 @@
 (ns pl.tomaszgigiel.abc-xml.core
   (:require [clojure.tools.logging :as log])
+  (:require [clojure.xml])
   (:gen-class))
+
+(defn abc-parse
+  [s]
+  (clojure.xml/parse s))
 
 (defn -main
   "abc-xml: xml in clojure"
--- src/test/clojure/pl/tomaszgigiel/abc_xml/core_test.clj
+++ src/test/clojure/pl/tomaszgigiel/abc_xml/core_test.clj
@@ -1,8 +1,17 @@
 (ns pl.tomaszgigiel.abc-xml.core-test
   (:use [clojure.test])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.edn :as edn])
   (:require [pl.tomaszgigiel.abc-xml.core :as abc])
   (:require [pl.tomaszgigiel.abc-xml.core-test-config :as mytest]))
 
 (use-fixtures :once mytest/once-fixture)
 (use-fixtures :each mytest/each-fixture)
 
+(deftest uri-string-test
+  (is (.contains (->> "simple.xml" io/resource str) "/abc-xml/src/test/resources/simple.xml")))
+
+(deftest parse-test
+  (let [expected  (->> "simple.xml.edn" io/resource slurp edn/read-string)
+        actual (abc/abc-parse (str (io/resource "simple.xml")))]
+  (is (= expected actual))))
